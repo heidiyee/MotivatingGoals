78540c54c4e662c1f11fcf0e9c4e3edf6634e57f
heidiyee <heidi.n.yee@gmail.com>
2016-01-20 14:42:41 -0800

    First commit

---
+++ MotivatingGoals/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MotivatingGoals
+//
+//  Created by Heidi Yee on 1/20/16.
+//  Copyright © 2016 Heidi Yee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
